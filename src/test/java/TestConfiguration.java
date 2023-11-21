572bac6eba74f5484514a4b87925c23210cba5c5
Makasharip <murtazaliev002@mail.ru>
2023-11-21 21:39:08 +0300

    changed version

--- src/test/java/TestConfiguration.java
+++ src/test/java/TestConfiguration.java
@@ -7,7 +7,7 @@ public class TestConfiguration {
         Configuration.baseUrl = System.getProperty("baseURL", "https://app.qase.io");
         Configuration.remote = System.getProperty("webDriverHost", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "114.0");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.timeout = 10000;
 
